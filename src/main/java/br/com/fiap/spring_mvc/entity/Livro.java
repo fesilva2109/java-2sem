8a8f6bd55491c2a668954049647d6992c2d2f749
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-08-08 22:32:12 -0300

    Colocando Validações na Classe Livro

--- src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
+++ src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
@@ -1,6 +1,7 @@
 package br.com.fiap.spring_mvc.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,12 +13,18 @@ public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank (message = "O título é obrigatório")
     private String titulo;
+    @NotBlank (message = "O autor é obrigatório")
     private String autor;
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "A categoria é obrigatório")
     private Categoria categoria;
     private String editora;
+    @DecimalMin(value = "0.99", message = "O preço deve ser no mínimo 0.99")
     private BigDecimal preco;
+    @Pattern(regexp = "^970\\d{7}$|^970\\d{10}$",
+            message = "ISBN fora do padrão")
     private String isbn;
     private LocalDate dataPublicacao;
 

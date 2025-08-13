8b16033c63afa1bbc84ab0fabdff85f047208132
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-08-12 22:03:07 -0300

    Alterando controller e livroLista.html

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -1,16 +1,26 @@
 package br.com.fiap.spring_mvc.controller;
 
+import br.com.fiap.spring_mvc.entity.Livro;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
     @GetMapping("/lista")
     public String listarLivros(Model model){
-        model
+        Livro livro1 = new Livro();
+        livro1.setId(1L);
+        livro1.setTitulo("Harry Potter");
+        Livro livro2 = new Livro();
+        livro2.setId(2L);
+        livro2.setTitulo("Percy Jackson");
+        List<Livro> livros = List.of(livro1, livro2);
+        model.addAttribute("listaLivros", livros);
         return "livroLista";
     }
 

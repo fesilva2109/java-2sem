8667c2c352cad29905e6e2ab2a8bb13cb13cc495
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-08-15 22:48:44 -0300

    Aula 15/08

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -1,17 +1,41 @@
 package br.com.fiap.spring_mvc.controller;
 
+import br.com.fiap.spring_mvc.entity.Categoria;
+import br.com.fiap.spring_mvc.entity.Livro;
+import br.com.fiap.spring_mvc.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
+    @Autowired
+    LivroService livroService;
+
     @GetMapping("/lista")
-    public String listarLivros(Model model){
-        model
+    public String listarLivros(Model model) {
+       List<Livro> livros = livroService.readLivros();
+       model.addAttribute("listaLivros", livros);
         return "livroLista";
     }
 
+    @GetMapping("/cadastro")
+    public String cadastroLivro(Model model){
+        model.addAttribute("livro", new Livro());
+        model.addAttribute("categoriaLista", Arrays.asList(Categoria.values()));
+        return "livroCadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarLivro(Livro livro, Model model){
+        livroService.createLivro(livro);
+        return listarLivros(model);
+    }
 }
